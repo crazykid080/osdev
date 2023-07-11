191a51791e30e3387e7827b72484a8572618d7c7
crazykid080 <crazykid080@osdev.org>
2023-07-10 23:31:47 -0700

    Setup file management and prep bootloader

--- makefile
+++ makefile
@@ -1,11 +1,17 @@
-ASM=nasm
+ASM:=nasm
+CC:=gcc
 
-SRC_DIR=src
-BUILD_DIR=build
+SRC_DIR:=src
+BUILD_DIR:=build
+TOOLS_DIR:=tools
+
+WARNINGS := -Wall -Wextra -pedantic
+CFLAGS := -g -std=c99 $(WARNINGS)
+
+.PHONY: all floppy_image kernel bootloader clean always tools_fat
 
 
-.PHONY: all floppy_image kernel bootloader clean always
-
+all: floppy_image tools_fat
 
 #
 # Floppy disk size
@@ -13,32 +19,40 @@ BUILD_DIR=build
 floppy_image: $(BUILD_DIR)/main_floppy.img
 
 $(BUILD_DIR)/main_floppy.img: bootloader kernel
-	dd if=/dev/zero of=$(BUILD_DIR)/main_floppy.img bs=512 count=2880
-	mkfs.fat -F 12 -n "NBOS" $(BUILD_DIR)/main_floppy.img
-	dd if=$(BUILD_DIR)/bootloader.bin of=$(BUILD_DIR)/main_floppy.img conv=notrunc
-	mcopy -i $(BUILD_DIR)/main_floppy.img $(BUILD_DIR)/kernel.bin "::kernel.bin"
+	@dd if=/dev/zero of=$(BUILD_DIR)/main_floppy.img bs=512 count=2880
+	@mkfs.fat -F 12 -n "NBOS" $(BUILD_DIR)/main_floppy.img
+	@dd if=$(BUILD_DIR)/bootloader.bin of=$(BUILD_DIR)/main_floppy.img conv=notrunc
+	@mcopy -i $(BUILD_DIR)/main_floppy.img $(BUILD_DIR)/kernel.bin "::kernel.bin"
+	@mcopy -i $(BUILD_DIR)/main_floppy.img test.txt "::test.txt"
 
 #
 # Bootloader
 #
 bootloader: $(BUILD_DIR)/bootloader.bin
 $(BUILD_DIR)/bootloader.bin: always
-	$(ASM) $(SRC_DIR)/bootloader/boot.asm -f bin -o $(BUILD_DIR)/bootloader.bin
+	@$(ASM) $(SRC_DIR)/bootloader/boot.asm -f bin -o $(BUILD_DIR)/bootloader.bin
 
 #
 # Kernel
 #
 kernel: $(BUILD_DIR)/kernel.bin
-
 $(BUILD_DIR)/kernel.bin: always
-	$(ASM) $(SRC_DIR)/kernel/main.asm -f bin -o $(BUILD_DIR)/kernel.bin
+	@$(ASM) $(SRC_DIR)/kernel/main.asm -f bin -o $(BUILD_DIR)/kernel.bin
+
+#
+# Tools
+#
+tools_fat: $(BUILD_DIR)/tools/fat
+$(BUILD_DIR)/tools/fat: always $(TOOLS_DIR)/fat/fat.c
+	@ mkdir -p $(BUILD_DIR)/tools
+	@ $(CC) $(CFLAGS) -g -o $(BUILD_DIR)/tools/fat $(TOOLS_DIR)/fat/fat.c
 
 
 #
 # always
 #
 always:
-	mkdir -p $(BUILD_DIR)
+	@mkdir -p $(BUILD_DIR)
 
 #
 # clean
--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -10,7 +10,7 @@ bits 16                     ;Sets instruction bit size as 16
 jmp short start
 nop
 
-bdb_oem:                    db 'CRAZY0.1'                   ;8 bytes
+bdb_oem:                    db 'ASYLUM01'                   ;8 bytes
 bdb_bytes_per_sector:       dw 512
 bdb_sectors_per_cluster:    db 1
 bdb_reserved_sectors:       dw 1
@@ -37,33 +37,8 @@ ebr_system_id:              db 'FAT12   '                   ; 8 bytes, pad with 
 
 ;goto start of program
 start:
-    jmp main
-
-; Will print a string to the screen.
-; Params:
-;   -ds:si pointer to string
-;   -
-puts:
-    push si
-    push ax
-
-.loop:
-    lodsb                   ; Loads next character in al
-    or al, al               ; verify if next character is null
-    jz .done
-    mov ah, 0x0e
-    mov bh, 0
-    int 0x10
-    jmp .loop
-
-.done:
-    pop ax
-    pop si
-    ret
-
-main:
     ; setup data segments
-    mov ax, 0
+    mov ax, 0                               ;Since we can't set ds and es directly, we need an intermediary register
     mov ds, ax
     mov es, ax
 
@@ -71,28 +46,170 @@ main:
     mov ss, ax
     mov sp, 0x7c00                          ;Set stack pointer to currrent address. Stack will grow downwards 
 
+    push es
+    push word .after
+    retf
+
+
+.after:
     ; read something from disk
     ; BIOS should set DL to drive number
     mov [ebr_drive_number], dl
 
-    mov ax, 1                               ; LBA[1], second sector from disk
-    mov cl, 1
-    mov bx, 0x7E00                          ; data should be AFTER bootloader 
-    call disk_read
-
-    mov si, msg_hello
+    mov si, msg_loading
     call puts
 
-    cli
-    hlt                                     ;halts the processor
+    ; read drive parameters (sectors per track and head count),
+    ; instead of relying on data on formatted disk
+    push es
+    mov ah, 08h
+    int 13h
+    jc floppy_error
+    pop es
 
-.halt:
-    cli                                     ; disable interrupts to prevent escaping halt state
+ and cl, 0x3F                        ; remove top 2 bits
+    xor ch, ch
+    mov [bdb_sectors_per_track], cx     ; sector count
+
+    inc dh
+    mov [bdb_heads], dh                 ; head count
+
+    ; compute LBA of root directory = reserved + fats * sectors_per_fat
+    ; note: this section can be hardcoded
+    mov ax, [bdb_sectors_per_fat]
+    mov bl, [bdb_fat_count]
+    xor bh, bh
+    mul bx                              ; ax = (fats * sectors_per_fat)
+    add ax, [bdb_reserved_sectors]      ; ax = LBA of root directory
+    push ax
+
+    ; compute size of root directory = (32 * number_of_entries) / bytes_per_sector
+    mov ax, [bdb_dir_entries_count]
+    shl ax, 5                           ; ax *= 32
+    xor dx, dx                          ; dx = 0
+    div word [bdb_bytes_per_sector]     ; number of sectors we need to read
+
+    test dx, dx                         ; if dx != 0, add 1
+    jz .root_dir_after
+    inc ax                              ; division remainder != 0, add 1
+                                        ; this means we have a sector only partially filled with entries
+.root_dir_after:
+
+    ; read root directory
+    mov cl, al                          ; cl = number of sectors to read = size of root directory
+    pop ax                              ; ax = LBA of root directory
+    mov dl, [ebr_drive_number]          ; dl = drive number (we saved it previously)
+    mov bx, buffer                      ; es:bx = buffer
+    call disk_read
+
+    ; search for kernel.bin
+    xor bx, bx
+    mov di, buffer
+
+.search_kernel:
+    mov si, file_kernel_bin
+    mov cx, 11                          ; compare up to 11 characters
+    push di
+    repe cmpsb
+    pop di
+    je .found_kernel
+
+    add di, 32
+    inc bx
+    cmp bx, [bdb_dir_entries_count]
+    jl .search_kernel
+
+    ; kernel not found
+    jmp kernel_not_found_error
+
+.found_kernel:
+
+    ; di should have the address to the entry
+    mov ax, [di + 26]                   ; first logical cluster field (offset 26)
+    mov [kernel_cluster], ax
+
+    ; load FAT from disk into memory
+    mov ax, [bdb_reserved_sectors]
+    mov bx, buffer
+    mov cl, [bdb_sectors_per_fat]
+    mov dl, [ebr_drive_number]
+    call disk_read
+
+    ; read kernel and process FAT chain
+    mov bx, KERNEL_LOAD_SEGMENT
+    mov es, bx
+    mov bx, KERNEL_LOAD_OFFSET
+
+.load_kernel_loop:
+    
+    ; Read next cluster
+    mov ax, [kernel_cluster]
+    
+    ; not nice :( hardcoded value
+    add ax, 31                          ; first cluster = (kernel_cluster - 2) * sectors_per_cluster + start_sector
+                                        ; start sector = reserved + fats + root directory size = 1 + 18 + 134 = 33
+    mov cl, 1
+    mov dl, [ebr_drive_number]
+    call disk_read
+
+    add bx, [bdb_bytes_per_sector]
+
+    ; compute location of next cluster
+    mov ax, [kernel_cluster]
+    mov cx, 3
+    mul cx
+    mov cx, 2
+    div cx                              ; ax = index of entry in FAT, dx = cluster mod 2
+
+    mov si, buffer
+    add si, ax
+    mov ax, [ds:si]                     ; read entry from FAT table at index ax
+
+    or dx, dx
+    jz .even
+
+.odd:
+    shr ax, 4
+    jmp .next_cluster_after
+
+.even:
+    and ax, 0x0FFF
+
+.next_cluster_after:
+    cmp ax, 0x0FF8                      ; end of chain
+    jae .read_finish
+
+    mov [kernel_cluster], ax
+    jmp .load_kernel_loop
+
+.read_finish:
+    
+    ; jump to our kernel
+    mov dl, [ebr_drive_number]          ; boot device in dl
+
+    mov ax, KERNEL_LOAD_SEGMENT         ; set segment registers
+    mov ds, ax
+    mov es, ax
+
+    jmp KERNEL_LOAD_SEGMENT:KERNEL_LOAD_OFFSET
+
+    jmp wait_key_and_reboot             ; should never happen
+
+    cli                                 ; disable interrupts, this way CPU can't get out of "halt" state
     hlt
 
 
+;
+; Error handlers
+;
+
 floppy_error:
     mov si, msg_read_failed
+    call puts
+    jmp wait_key_and_reboot
+
+kernel_not_found_error:
+    mov si, msg_kernel_not_found
     call puts
     jmp wait_key_and_reboot
 
@@ -101,6 +218,29 @@ wait_key_and_reboot:
     int 16h                     ; wait for keypress
     jmp 0FFFFh:0                ; jump to beginning of BIOS. Should force a reboot
 
+
+puts:
+    ; save registers we will modify
+    push si
+    push ax
+    push bx
+
+.loop:
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
+    jz .done
+
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
+    int 0x10
+
+    jmp .loop
+
+.done:
+    pop bx
+    pop ax
+    pop si    
+    ret
 
 ;
 ; Disk routines
@@ -211,8 +351,16 @@ disk_reset:
     ret
 
 
-msg_hello:              db 'IM VM RICKKKK', ENDL, 0
-msg_read_failed:        db 'Read from disk failed', ENDL, 0 
+msg_loading:            db 'Loading', ENDL, 0
+msg_read_failed:        db 'Disk read failed', ENDL, 0 
+msg_kernel_not_found:   db 'KERNEL file not found!', ENDL, 0
+file_kernel_bin:        db 'KERNEL  BIN'
+kernel_cluster:         dw 0
+
+KERNEL_LOAD_SEGMENT     equ 0x2000
+KERNEL_LOAD_OFFSET      equ 0
 
 times 510-($-$$) db 0       ;($-$$) = size of program so far. This is the setup to set the last two bytes as needed
-dw 0AA55h                   ;Set bytes to declare this as bootableS
+dw 0AA55h                   ;Set bytes to declare this as bootable
+
+buffer:
