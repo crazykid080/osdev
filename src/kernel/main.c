e725e591b6a9c01c019f468b32e8418e927841b6
crazykid080 <crazykid080@osdev.org>
2023-07-31 08:55:19 -0700

    Added timer handler. I'm about to go off the beaten path and work on keyboard stuff. I don't know how this will go

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -2,15 +2,23 @@
 #include "stdio.h"
 #include "memory.h"
 #include "hal/hal.h"
+#include "arch/i686/irq.h"
 
 extern uint8_t __bss_start;
 extern uint8_t __end;
 
 void crash_me();
 
+void timer(Registers* regs){
+    printf("Tick!");
+}
+
 void __attribute__((section(".entry"))) start(uint16_t bootDrive){
     memset(&__bss_start, 0, (&__end) - (&__bss_start));
+    
     HAL_initialize();
+    i686_IRQ_RegisterHandler(0, timer);
+
     printf("Kernel started!\n");
     clrscr();
 
