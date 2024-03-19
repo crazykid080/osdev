bf94d30bf945330f491ca2563632435c533010f5
crazykid080 <crazykid080@osdev.org>
2024-03-18 18:26:12 -0700

    Ignore E0 iterrupts for now.

--- src/kernel/arch/i686/handlers/keyboard/keyboard.c
+++ src/kernel/arch/i686/handlers/keyboard/keyboard.c
@@ -8,19 +8,19 @@ void keyHandler(Registers* regs){
     uint16_t recv = i686_inb(0x60);
     uint16_t scancode = recv - 1;
     uint16_t keypress = recv & ~0x80;
-
+    if(recv == 0xE0){
+        //Extended byte... continue for now, no need to worry about it.
+        return;
     if(keypress > (sizeof(qwerty_Scancodes)/sizeof(qwerty_Scancodes[0]))){ //Gets the length of the array instead of size
         log_err("Keyboard handler", "ERROR: input 0x%x exceeds size of scancode table", recv);
         return;
     }
-    if(recv == 0xE0){
-        //Extended byte... wait for nexr interrupt... how?
-    }
-    if(recv < 0x80){
+    
+    }else if(recv < 0x80){
         log_debug("Keyboard handler", "Based on scancode table, you pressed key %s", qwerty_Scancodes[scancode]);
         return;
     } else {
-        log_debug("Keyboard handler", "SCANCODE DEBUG: 0x%x", (recv^0x80));
+        //log_debug("Keyboard handler", "SCANCODE DEBUG: 0x%x", (recv^0x80));
         log_debug("Keyboard handler", "Based on scancode table, you released key %s", qwerty_Scancodes[(recv ^ 0x80)-1]);
         return;
     }
